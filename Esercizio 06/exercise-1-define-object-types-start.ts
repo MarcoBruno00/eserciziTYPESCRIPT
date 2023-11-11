d68fe0aec0215343a499357a46f890abcddab156
marco bruno <marco2000bruno@gmail.com>
2023-11-11 15:55:40 +0100

    esercizio6

--- Esercizio 06/exercise-1-define-object-types-start.ts
+++ Esercizio 06/exercise-1-define-object-types-start.ts
@@ -4,13 +4,18 @@
 
 // Add a type annotation to this variable that describes the object it contains.
 
-const countryPopulation = {
+const countryPopulation: { code: string, population: number } = {
     code: "NZ",
     population: 5_135_300,
 };
 
 // Define a `Country` interface that describes the object in this variable.
 // Add the `Country` type as a type annotation for this variable.
+interface countryData {
+    name: "India",
+    code: "IN",
+    population: 1_352_642_280,
+}
 
 const countryData = {
     name: "India",
@@ -20,6 +25,11 @@ const countryData = {
 
 // Define a `Currency` type alias that describes the object in this variable.
 // Add the `Currency` type as a type annotation for this variable.
+type currencyData = {
+    name: "Euro",
+    code: "EUR",
+    symbol: "€",
+}
 
 const currencyData = {
     name: "Euro",
@@ -29,4 +39,4 @@ const currencyData = {
 
 // ----
 
-export {};
+export { };
